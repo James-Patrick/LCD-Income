bfd6db7734ef5d6e8773524cc2416a700c26007e
unknown <20947715@ECM-C-W-207-005.uniwa.uwa.edu.au>
2015-10-13 11:34:30 +0800

    added adult dataset

---
+++ test.py
@@ -0,0 +1,28 @@
+class Classifier:
+	def __init__(self):
+		self.data = []
+		self.rule = None
+		self.action = None
+	
+	
+	
+
+
+def read_data():
+	file = open("adult.data", "r")
+	
+
+
+	
+	
+	
+	
+	
+classifiers = []
+
+for i in range(10):
+	c = Classifier()
+	classifiers.append(c)
+
+for c in classifiers:
+	print(c)
