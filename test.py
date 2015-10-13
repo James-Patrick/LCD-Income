e14e030d2d3d84f847dc81a65aa457fcc6b004f7
unknown <20947715@ECM-C-W-207-005.uniwa.uwa.edu.au>
2015-10-13 13:40:55 +0800

    working on environment

--- test.py
+++ test.py
@@ -1,14 +1,33 @@
-filename = 'adults.data'
-headings = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'salary']
+FILENAME = 'adult.data'
+HEADINGS_DICT = {'age': 'continuous', 
+			'workclass': 'discrete',
+			'fnlwgt': 'continuous',
+			'education':'discrete',
+			'education-num': 'continuous',
+			'marital-status': 'discrete',
+			'occupation':'discrete',
+			'relationship':'discrete',
+			'race':'discrete',
+			'sex':'discrete',
+			'capital-gain':'continuous',
+			'capital-loss':'continuous',
+			'hours-per-week':'continuous',
+			'native-country':'discrete',
+			'salary':'class-label'}
 
+HEADINGS = [
 
 
 # A classifier. 
 class Classifier:
 	def __init__(self):
 		self.data = []
-		self.rule = None
+		self.condition = None
 		self.action = None
+		self.rule = [self.condition, self.action]
+	
+	def generate_random_rule():
+		print('hi')
 		
 		
 # One environment (a dictionary that maps field names to their values).	
@@ -16,29 +35,47 @@ class Classifier:
 #   {'education': 'HS-grad', 'workclass': 'Local-gov', 'age': '67' ...}
 class Environment:
 	def __init__(self, data):
-		self.dictionary = {headings[h]: data[h] for h in range(len(headings))}
+		# Note: make continuous stuff
 		
+		self.dictionary = {}
+
+		self.dictionary = {HEADINGS[h]: data[h] for h in range(len(HEADINGS))}
 		
+	
+	
 		
 # Returns list of environments
 def create_environments():	
-	with open("adult.data", "r") as file:		
+	with open(FILENAME, "r") as file:		
 		data = [f.replace(' ', '').rstrip().split(',') for f in file.readlines()]
 	
 	return [Environment(d) for d in data]
 	
+	
+	
 
+# Creates the initial population of classifiers, in the form of a list.
 def create_classifiers():
 	print('Hi')
 	
 		
 
+def main():
 
-environments = create_environments()
+	environments = create_environments()
+	classifiers = create_classifiers()	
 
+if __name__ == "__main__":
+    main()	
+	
+'''
 for e in environments:
 	print(e.dictionary)
+'''
 	
+
+	
+'''	
 classifiers = []
 
 for i in range(10):
@@ -47,3 +84,4 @@ for i in range(10):
 
 for c in classifiers:
 	print(c)
+'''
